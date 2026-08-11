99fda7e44c034f952451e6374c57ea6896fc315b
LilGazi <godslovemarcelo@gmail.com>
2026-08-12 00:38:24 +0100

    Implement password generator
    Added a password generator that prompts user for length and generates a random password using letters and digits.

--- password-generator.py
+++ password-generator.py
@@ -1,1 +1,22 @@
+import random
+import string
 
+rerun = True
+
+while rerun:
+    length = int(input("Enter password length: "))
+
+    characters = string.ascii_letters + string.digits
+
+    password = ""
+
+    for i in range(length):
+        password += random.choice(characters)
+
+    print("Your generated password is:", password)
+
+    choice = input("Do you want to continue? ").lower()
+
+    if choice != "yes":
+        rerun = False
+        print("Goodbye!")
